328f0c294abb1f4cb80ff65b1dcea55b3d7b08a4
Bryce Frazier <bx.frazier@gmail.com>
2018-02-23 10:58:50 -0800

    14 passing tests

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -15,4 +15,16 @@ class Volunteer
   def ==(another_volunteer)
     self.name().==(another_volunteer.name()).& self.id().==(another_volunteer.id())
   end
+
+  def self.all
+    volunteers = []
+    returned_volunteers = DB.exec("SELECT * FROM volunteer;")
+    returned_volunteers.each do |volunteer|
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id")
+      project_id = volunteer.fetch("project_id")
+      volunteers.push(Volunteer.new({:name => name, :id => id, :project_id => project_id}))
+    end
+    volunteers
+  end
 end
