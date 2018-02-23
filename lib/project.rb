401f866202a29e3fb54f57518004120040115a19
Bryce Frazier <bx.frazier@gmail.com>
2018-02-23 09:09:36 -0800

    3 passing tests

--- lib/project.rb
+++ lib/project.rb
@@ -5,5 +5,20 @@ class Project
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
   end
-  
+
+  def self.all
+    projects = []
+    returned_projects = DB.exec("SELECT * FROM project;")
+    returned_projects.each() do |list|
+      title = title.fetch(:title)
+      id = id.fetch(:id)
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
+  def save
+    result = DB.exec("INSERT INTO project (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
 end
